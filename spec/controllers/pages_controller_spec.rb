cd32f2d784347e3da155833a9a9d57ec6f055369
Julian Hentea <j.g.hentea@gmail.com>
2013-06-13 15:54:44 -0600

    addred links and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -27,10 +27,18 @@ describe PagesController do
       response.should be_success
     end
   end
+  
   describe "GET 'about'" do
      it "returns http success" do
        get 'about'
        response.should be_success
      end
    end
+   describe "GET 'help'" do
+      it "returns http success" do
+        get 'help'
+        response.should be_success
+      end
+    end
+
 end
