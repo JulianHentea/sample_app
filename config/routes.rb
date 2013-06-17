2b93fc01f3218584f0424df19e4c025a853b445c
Julian Hentea <j.g.hentea@gmail.com>
2013-06-17 17:18:59 -0600

    Working user signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 SampleApp::Application.routes.draw do
 
-  resources :users
-  resources :sessions, :only => [:new, :create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   
-  root :to => 'pages#home'
+  resources :sessions,      :only => [:new, :create, :destroy]
+  resources :microposts,    :only => [:create, :destroy]
+  resources :relationships, :only => [:create, :destroy]
   
-  match '/contact',   :to => 'pages#contact'
-  match '/about',     :to => 'pages#about'
-  match '/help',      :to => 'pages#help'
-  match '/signup',    :to => 'users#new'
-  match '/signin',    :to => 'sessions#new'
-  match '/signout',   :to => 'sessions#destroy'
-  
+  root :to => "pages#home"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
